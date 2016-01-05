1e5551bbd05097976f00c7406ee8ca99fb7c7556
Daniel Perelman <perelman@cs.washington.edu>
2016-01-05 08:46:31 -0800

    Attempt to make sqlite work better. Might have to switch to Postgres.

--- avalon/avalon/settings.py
+++ avalon/avalon/settings.py
@@ -80,6 +80,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ATOMIC_REQUESTS': True,
+        'OPTIONS': {
+            'timeout': 60, # longer database timeout
+        }
     }
 }
 
