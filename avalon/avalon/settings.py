876994ad09888fbc8fc19e2eef8ed4cef2f357b9
Daniel Perelman <perelman@cs.washington.edu>
2016-01-07 02:47:19 -0800

    Fix for Django <1.8 support.

--- avalon/avalon/settings.py
+++ avalon/avalon/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE_CLASSES = [
-    'django.middleware.security.SecurityMiddleware',
+    # Does not exist in Django before 1.8.
+    #'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
