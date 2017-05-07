917a83c7875532f6124e0b5ba2e0df4948964095
Daniel Perelman <perelman@cs.washington.edu>
2017-05-07 14:35:08 -0700

    Updated New Game and Join Game pages/forms so observe button requires name to be blank and joining as a player requires name to be non-blank.

--- avalon/avalon_game/forms.py
+++ avalon/avalon_game/forms.py
@@ -3,12 +3,23 @@ from django import forms
 from .models import Game, Player
 
 class NewGameForm(forms.Form):
-    name = forms.CharField(label='Name', max_length=80)
+    name = forms.CharField(label='Name', max_length=80, required=False)
+
+    def clean(self):
+        cleaned_data = super(NewGameForm, self).clean()
+
+        name = cleaned_data.get("name")
+        observer = 'observe' in self.data
+
+        if observer and (name is None or len(name) > 0):
+            self.add_error('name', "Only fill in 'Name' field if you will be playing (not observing) using this device.")
+        elif not observer and len(name) == 0:
+            self.add_error('name', "Player name must be non-empty (did you mean to click 'Create as observer'?).")
 
 class JoinGameForm(forms.Form):
     game = forms.CharField(label='Access code',
                            max_length=Game.ACCESS_CODE_LENGTH)
-    player = forms.CharField(label='Name', max_length=80)
+    player = forms.CharField(label='Name', max_length=80, required=False)
 
     def clean_game(self):
         data = self.cleaned_data['game']
@@ -25,6 +36,12 @@ class JoinGameForm(forms.Form):
         name = cleaned_data.get("player")
         observer = 'observe' in self.data
         cleaned_data["observer"] = observer
+
+        if observer and (name is None or len(name) > 0):
+            self.add_error('player', "Leave 'Name' field blank if observing or use 'Join' button to join as a player.")
+        elif not observer and len(name) == 0:
+            self.add_error('player', "Player name must be non-empty (did you mean to click 'Observe'?).")
+            return
 
         if game is None or name is None or observer:
             cleaned_data["player"] = None
